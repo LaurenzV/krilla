d012427bf229c8ea098c2e85a2e05d02dabbd214
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2024-07-19 15:22:42 +0200

    Destroy temporarily

--- src/object/tiling_pattern.rs
+++ src/object/tiling_pattern.rs
@@ -24,37 +24,38 @@ impl TilingPattern {
 
 impl Object for TilingPattern {
     fn serialize_into(self, sc: &mut SerializerContext, root_ref: Ref) {
-        let mut chunk = Chunk::new();
-        // TODO: Deduplicate.
-
-        let mut resource_dictionary = ResourceDictionary::new();
-        let (content_stream, bbox) = {
-            let mut serializer = CanvasPdfSerializer::new(&mut resource_dictionary);
-            serializer.serialize_instructions(self.0.canvas.byte_code.instructions());
-            serializer.finish()
-        };
-
-        let mut tiling_pattern = chunk.tiling_pattern(root_ref, &content_stream);
-        resource_dictionary.to_pdf_resources(sc, &mut tiling_pattern.resources());
-
-        let final_bbox = pdf_writer::Rect::new(
-            0.0,
-            0.0,
-            self.0.canvas.size.width(),
-            self.0.canvas.size.height(),
-        );
-
-        tiling_pattern
-            .tiling_type(TilingType::ConstantSpacing)
-            .paint_type(PaintType::Colored)
-            .bbox(final_bbox)
-            .matrix(self.0.transform.0.to_pdf_transform())
-            .x_step(final_bbox.x2 - final_bbox.x1)
-            .y_step(final_bbox.y2 - final_bbox.y1);
-
-        tiling_pattern.finish();
-
-        sc.chunk_mut().extend(&chunk);
+        todo!()
+        // let mut chunk = Chunk::new();
+        // // TODO: Deduplicate.
+        //
+        // let mut resource_dictionary = ResourceDictionary::new();
+        // let (content_stream, bbox) = {
+        //     let mut serializer = CanvasPdfSerializer::new(&mut resource_dictionary);
+        //     serializer.serialize_instructions(self.0.canvas.byte_code.instructions());
+        //     serializer.finish()
+        // };
+        //
+        // let mut tiling_pattern = chunk.tiling_pattern(root_ref, &content_stream);
+        // resource_dictionary.to_pdf_resources(sc, &mut tiling_pattern.resources());
+        //
+        // let final_bbox = pdf_writer::Rect::new(
+        //     0.0,
+        //     0.0,
+        //     self.0.canvas.size.width(),
+        //     self.0.canvas.size.height(),
+        // );
+        //
+        // tiling_pattern
+        //     .tiling_type(TilingType::ConstantSpacing)
+        //     .paint_type(PaintType::Colored)
+        //     .bbox(final_bbox)
+        //     .matrix(self.0.transform.0.to_pdf_transform())
+        //     .x_step(final_bbox.x2 - final_bbox.x1)
+        //     .y_step(final_bbox.y2 - final_bbox.y1);
+        //
+        // tiling_pattern.finish();
+        //
+        // sc.chunk_mut().extend(&chunk);
     }
 
     fn is_cached(&self) -> bool {
--- src/object/xobject.rs
+++ src/object/xobject.rs
@@ -27,39 +27,40 @@ impl XObject {
 
 impl Object for XObject {
     fn serialize_into(self, sc: &mut SerializerContext, root_ref: Ref) {
-        let srgb_ref = sc.srgb();
-
-        let mut chunk = Chunk::new();
-
-        // TODO: Deduplicate
-        let mut resource_dictionary = ResourceDictionary::new();
-        let (content_stream, bbox) = {
-            let mut serializer = CanvasPdfSerializer::new(&mut resource_dictionary);
-            serializer.serialize_instructions(self.0.canvas.byte_code.instructions());
-            serializer.finish()
-        };
-
-        let mut x_object = chunk.form_xobject(root_ref, &content_stream);
-        resource_dictionary.to_pdf_resources(sc, &mut x_object.resources());
-        x_object.bbox(bbox.to_pdf_rect());
-
-        if self.0.isolated || self.0.transparency_group_color_space {
-            let mut transparency = x_object.group().transparency();
-
-            if self.0.isolated {
-                transparency.isolated(self.0.isolated);
-            }
-
-            if self.0.transparency_group_color_space {
-                transparency.pair(Name(b"CS"), srgb_ref);
-            }
-
-            transparency.finish();
-        }
-
-        x_object.finish();
-
-        sc.chunk_mut().extend(&chunk);
+        todo!();
+        // let srgb_ref = sc.srgb();
+        //
+        // let mut chunk = Chunk::new();
+        //
+        // // TODO: Deduplicate
+        // let mut resource_dictionary = ResourceDictionary::new();
+        // let (content_stream, bbox) = {
+        //     let mut serializer = CanvasPdfSerializer::new(&mut resource_dictionary);
+        //     serializer.serialize_instructions(self.0.canvas.byte_code.instructions());
+        //     serializer.finish()
+        // };
+        //
+        // let mut x_object = chunk.form_xobject(root_ref, &content_stream);
+        // resource_dictionary.to_pdf_resources(sc, &mut x_object.resources());
+        // x_object.bbox(bbox.to_pdf_rect());
+        //
+        // if self.0.isolated || self.0.transparency_group_color_space {
+        //     let mut transparency = x_object.group().transparency();
+        //
+        //     if self.0.isolated {
+        //         transparency.isolated(self.0.isolated);
+        //     }
+        //
+        //     if self.0.transparency_group_color_space {
+        //         transparency.pair(Name(b"CS"), srgb_ref);
+        //     }
+        //
+        //     transparency.finish();
+        // }
+        //
+        // x_object.finish();
+        //
+        // sc.chunk_mut().extend(&chunk);
     }
 
     fn is_cached(&self) -> bool {
