b52bba08028d2ff979a977834f439de7a2fa9de8
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2024-08-29 20:50:56 +0200

    Remove comment

--- krilla-macros/tests/test.rs
+++ krilla-macros/tests/test.rs
@@ -1,15 +1,1 @@
-use krilla_macros::snapshot;
-
-// #[snapshot("cid_font/hello_world")]
-// fn hi_there(sc: &mut SerializerContext) {
-//     let font_data = Arc::new(load_font("NotoSans-Regular.ttf"));
-//     let font = Font::new(font_data, 0, Location::default()).unwrap();
-//     sc.create_or_get_font_container(font.clone())
-//         .borrow_mut()
-//         .add_glyph(GlyphId::new(36));
-//     sc.create_or_get_font_container(font.clone())
-//         .borrow_mut()
-//         .add_glyph(GlyphId::new(37));
-// }
-
 fn main() {}
