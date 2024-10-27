b7609a73a654e9db8fb30519538b9100743d2345
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2024-10-27 17:13:21 +0100

    Re-add snippet from simple-text

--- examples/simple_text.rs
+++ examples/simple_text.rs
@@ -8,13 +8,13 @@
 
 use krilla::color::rgb;
 use krilla::font::Font;
+use krilla::geom::Point;
 use krilla::paint::{LinearGradient, SpreadMethod, Stop};
-use krilla::path::Fill;
+use krilla::path::{Fill, Stroke};
 use krilla::surface::TextDirection;
 use krilla::{Document, PageSettings};
 use std::sync::Arc;
-use tiny_skia_path::Point;
-use usvg::NormalizedF32;
+use krilla::geom::NormalizedF32;
 
 fn main() {
     // The usual page setup.
@@ -66,79 +66,79 @@ fn main() {
         false,
         TextDirection::Auto,
     );
-    //
-    // // Instead of applying fills, we can also apply strokes!
-    // surface.stroke_text(
-    //     Point::from_xy(0.0, 50.0),
-    //     Stroke {
-    //         paint: rgb::Color::new(0, 255, 0).into(),
-    //         ..Default::default()
-    //     },
-    //     noto_font.clone(),
-    //     25.0,
-    //     &[],
-    //     "This text is stroked green!",
-    //     false,
-    //     TextDirection::Auto,
-    // );
-    //
-    // let noto_arabic_font = Font::new(
-    //     Arc::new(std::fs::read("assets/fonts/NotoSansArabic-Regular.ttf").unwrap()),
-    //     0,
-    //     vec![],
-    // )
-    // .unwrap();
-    //
-    // // As mentioned above, complex scripts are supported, you just can't mix them in
-    // // one run.
-    // surface.fill_text(
-    //     Point::from_xy(0.0, 75.0),
-    //     Fill::default(),
-    //     noto_arabic_font.clone(),
-    //     25.0,
-    //     &[],
-    //     "هذا هو السطر الثاني من النص.",
-    //     false,
-    //     TextDirection::Auto,
-    // );
-    //
-    // let mut y = 125.0;
-    //
-    // let axes = vec![
-    //     vec![("wght".to_string(), 100.0), ("wdth".to_string(), 62.5)],
-    //     vec![("wght".to_string(), 233.33), ("wdth".to_string(), 68.75)],
-    //     vec![("wght".to_string(), 366.67), ("wdth".to_string(), 75.0)],
-    //     vec![("wght".to_string(), 500.0), ("wdth".to_string(), 81.25)],
-    //     vec![("wght".to_string(), 633.33), ("wdth".to_string(), 87.5)],
-    //     vec![("wght".to_string(), 766.67), ("wdth".to_string(), 93.75)],
-    //     vec![("wght".to_string(), 900.0), ("wdth".to_string(), 100.0)],
-    // ];
-    //
-    // // krilla also supports variable fonts!
-    // for coordinates in axes {
-    //     let noto_font_variable = Font::new(
-    //         Arc::new(std::fs::read("assets/fonts/NotoSans_variable.ttf").unwrap()),
-    //         0,
-    //         coordinates,
-    //     )
-    //     .unwrap();
-    //
-    //     surface.fill_text(
-    //         Point::from_xy(0.0, y),
-    //         Fill {
-    //             paint: rgb::Color::new(0, 0, 255).into(),
-    //             ..Default::default()
-    //         },
-    //         noto_font_variable.clone(),
-    //         25.0,
-    //         &[],
-    //         "Variable fonts rock!",
-    //         false,
-    //         TextDirection::Auto,
-    //     );
-    //
-    //     y += 25.0;
-    // }
+
+    // Instead of applying fills, we can also apply strokes!
+    surface.stroke_text(
+        Point::from_xy(0.0, 50.0),
+        Stroke {
+            paint: rgb::Color::new(0, 255, 0).into(),
+            ..Default::default()
+        },
+        noto_font.clone(),
+        25.0,
+        &[],
+        "This text is stroked green!",
+        false,
+        TextDirection::Auto,
+    );
+
+    let noto_arabic_font = Font::new(
+        Arc::new(std::fs::read("assets/fonts/NotoSansArabic-Regular.ttf").unwrap()),
+        0,
+        vec![],
+    )
+    .unwrap();
+
+    // As mentioned above, complex scripts are supported, you just can't mix them in
+    // one run.
+    surface.fill_text(
+        Point::from_xy(0.0, 75.0),
+        Fill::default(),
+        noto_arabic_font.clone(),
+        25.0,
+        &[],
+        "هذا هو السطر الثاني من النص.",
+        false,
+        TextDirection::Auto,
+    );
+
+    let mut y = 125.0;
+
+    let axes = vec![
+        vec![("wght".to_string(), 100.0), ("wdth".to_string(), 62.5)],
+        vec![("wght".to_string(), 233.33), ("wdth".to_string(), 68.75)],
+        vec![("wght".to_string(), 366.67), ("wdth".to_string(), 75.0)],
+        vec![("wght".to_string(), 500.0), ("wdth".to_string(), 81.25)],
+        vec![("wght".to_string(), 633.33), ("wdth".to_string(), 87.5)],
+        vec![("wght".to_string(), 766.67), ("wdth".to_string(), 93.75)],
+        vec![("wght".to_string(), 900.0), ("wdth".to_string(), 100.0)],
+    ];
+
+    // krilla also supports variable fonts!
+    for coordinates in axes {
+        let noto_font_variable = Font::new(
+            Arc::new(std::fs::read("assets/fonts/NotoSans_variable.ttf").unwrap()),
+            0,
+            coordinates,
+        )
+        .unwrap();
+
+        surface.fill_text(
+            Point::from_xy(0.0, y),
+            Fill {
+                paint: rgb::Color::new(0, 0, 255).into(),
+                ..Default::default()
+            },
+            noto_font_variable.clone(),
+            25.0,
+            &[],
+            "Variable fonts rock!",
+            false,
+            TextDirection::Auto,
+        );
+
+        y += 25.0;
+    }
 
     surface.finish();
     page.finish();
