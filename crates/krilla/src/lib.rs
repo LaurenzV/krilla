7aae77fb27712b72523649a36cbc65ab8df1cf2d
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2024-11-30 12:53:02 +0100

    another fix...

--- crates/krilla/src/lib.rs
+++ crates/krilla/src/lib.rs
@@ -40,7 +40,7 @@ let mut font = {
     let path =
         PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../../assets/fonts/NotoSans-Regular.ttf");
     let data = std::fs::read(&path).unwrap();
-    Font::new(Arc::new(data), 0).unwrap()
+    Font::new(Arc::new(data), 0, true).unwrap()
 };
 
 // Add a new page with dimensions 200x200.
