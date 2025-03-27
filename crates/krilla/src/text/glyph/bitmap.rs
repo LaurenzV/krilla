3c1c91ebc3621ee4d94be67c67e44f79e02d8503
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-03-27 10:31:30 +0100

    Use the `glyph_for_size` method when selecting bitmap emojis (#175)

--- crates/krilla/src/text/glyph/bitmap.rs
+++ crates/krilla/src/text/glyph/bitmap.rs
@@ -15,10 +15,8 @@ pub(crate) fn draw_glyph(font: Font, glyph: GlyphId, surface: &mut Surface) -> O
 
     let bitmap_strikes = BitmapStrikes::new(font.font_ref());
 
-    let bitmap_glyph = bitmap_strikes
-        .iter()
-        .filter_map(|s| s.get(glyph.to_skrifa()))
-        .last()?;
+    let bitmap_glyph =
+        bitmap_strikes.glyph_for_size(skrifa::instance::Size::unscaled(), glyph.to_skrifa())?;
     let upem = metrics.units_per_em as f32;
 
     match bitmap_glyph.data {
