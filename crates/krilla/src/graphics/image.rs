7f13c65a19d861937e7f0c161c3ff29c27321294
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-10-31 14:56:08 +0100

    Don't unwrap invalid images, part 2

--- crates/krilla/src/graphics/image.rs
+++ crates/krilla/src/graphics/image.rs
@@ -548,9 +548,9 @@ fn decode_png(data: &[u8]) -> Option<Repr> {
     let cursor = Cursor::new(data);
     let mut decoder = png::Decoder::new(cursor);
     decoder.set_transformations(PNG_TRANSFORMATIONS);
-    let mut reader = decoder.read_info().unwrap();
+    let mut reader = decoder.read_info().ok()?;
     let mut img_data = vec![0; reader.output_buffer_size()];
-    let _ = reader.next_frame(&mut img_data).unwrap();
+    let _ = reader.next_frame(&mut img_data).ok()?;
     let (color_type, bit_depth) = reader.output_color_type();
 
     let color_space = match color_type {
