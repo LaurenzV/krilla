78e5251d266f248f4f212b94b78d5fe8b0f0ae29
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-11-10 21:18:17 +0100

    Apply layer isolation in the correct order

--- crates/krilla-svg/src/group.rs
+++ crates/krilla-svg/src/group.rs
@@ -1,6 +1,6 @@
 use krilla::num::NormalizedF32;
 use krilla::surface::Surface;
-use usvg::Node;
+use usvg::{BlendMode, Node, Opacity};
 
 use crate::util::{convert_blend_mode, UsvgTransformExt};
 use crate::{clip_path, filter, image, mask, path, text, ProcessContext};
@@ -17,11 +17,6 @@ pub(crate) fn render(
 
     let mut pop_count = 0;
 
-    if group.isolate() {
-        surface.push_isolated();
-        pop_count += 1
-    }
-
     surface.push_transform(&group.transform().to_krilla());
     pop_count += 1;
 
@@ -32,10 +27,27 @@ pub(crate) fn render(
     if let Some(mask) = group.mask() {
         pop_count += mask::render(mask, surface, process_context);
     }
+    
 
     surface.push_blend_mode(convert_blend_mode(&group.blend_mode()));
     surface.push_opacity(NormalizedF32::new(group.opacity().get()).unwrap());
     pop_count += 2;
+
+    // There is the `group.should_isolate` method which also returns true if
+    // there is a clip path, since strictly speaking, just a clip path
+    // also requires layer isolation. However, clip paths are very common
+    // and it would be wasteful to generate new isolated XObjects for that
+    // (especially since it shouldn't make any visual difference), so we
+    // don't include this here.
+    let should_isolate = group.isolate()
+        || group.opacity() != Opacity::ONE
+        || group.mask().is_some()
+        || group.blend_mode() != BlendMode::Normal;
+    
+    if should_isolate {
+        surface.push_isolated();
+        pop_count += 1
+    }
 
     for child in group.children() {
         render_node(child, surface, process_context);
